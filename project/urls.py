7828e09394b17a25f24e0f9ff19c7ab929cdce17
aaronn_t <75927372+aaronn44@users.noreply.github.com>
2022-06-14 10:42:43 +0430

    display profile picture

--- project/urls.py
+++ project/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
@@ -45,3 +47,6 @@ urlpatterns = [
     #      name="password_reset"),
     path("m/", include("msgr.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
